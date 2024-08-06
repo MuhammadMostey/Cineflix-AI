591f2d2c2ed0709b9b5827659ab77fd264a2db78
Muhammad Mostey <muhammadmostey@gmail.com>
2024-08-06 17:50:50 +0300

    updating styles to return object of styles

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,47 +1,21 @@
-
-import { useTheme } from "@mui/material/styles";
-import { createTheme } from "@mui/material/styles";
-// export const NavbarStyles = () => {
-//   const theme = useTheme();
-//   return {
-//     toolbar: {
-//       height: "80px",
-//       display: "flex",
-//       justifyContent: "space-between",
-//       marginLeft: "240px",
-//       [theme.breakpoints.down("sm")]: {
-//         marginLeft: "0",
-//         flexWrap: "wrap",
-//       },
-//     },
-//     menuButton: {
-//       marginRight: theme.spacing(2),
-//       [theme.breakpoints.up("sm")]: {
-//         display: "none",
-//       },
-//     },
-//     linkButton: {},
-//   };
-// };
-
-const theme = useTheme();
-
-export const NavbarStyles = createTheme({
-  toolbar: {
-    height: "80px",
-    display: "flex",
-    justifyContent: "space-between",
-    marginLeft: "240px",
-    [theme.breakpoints.down("sm")]: {
-      marginLeft: "0",
-      flexWrap: "wrap",
+export const NavbarStyles = (theme) => {
+  return {
+    toolbar: {
+      height: "80px",
+      display: "flex",
+      justifyContent: "space-between",
+      marginLeft: "240px",
+      [theme.breakpoints.down("sm")]: {
+        marginLeft: "0",
+        flexWrap: "wrap",
+      },
     },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up("sm")]: {
-      display: "none",
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
+      },
     },
-  },
-  linkButton: {},
-});
+    linkButton: {},
+  };
+};
