a915faca874efeaecfe6505e15f6d85ccaabe40d
Muhammad Mostey <muhammadmostey@gmail.com>
2024-08-06 17:51:25 +0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Movies } from "./Movies/Movies";
 export { default as Profile } from "./Profile/Profile";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as mainRoutes } from "./Routes/Routes";
+export { default as RoutesNew } from "./Routes/RoutesNew";
